fbf47cec31c506c8e88abecf9380c91283430285
Johan Westö <johan.westo@gmail.com>
2024-11-18 14:51:59 +0200

    minor cleanup

--- src/forecaster/plotting/static.py
+++ src/forecaster/plotting/static.py
@@ -1,8 +1,27 @@
-from collections import defaultdict
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from ..utils import save_figure
+
+from collections import defaultdict
+
+from forecaster.utils import get_safe_filename
 from forecaster.models.evaluate import calculate_price_extremes, calculate_prediction_accuracy
+
+def save_figure(fig, filename, save_dir):
+    """
+    Saves a figure to a file.
+
+    Args:
+    - fig (matplotlib.figure.Figure): The figure to save.
+    - filename (str): The name of the file to save the figure to.
+    - save_dir (str): The directory to save the figure to (default is None).
+    """
+    filename = get_safe_filename(filename)
+    if save_dir is not None:
+        save_path = os.path.join(save_dir, filename)
+    else:
+        save_path = filename
+    fig.savefig(save_path, bbox_inches='tight')
 
 def plot_time_coefficients(coefficients, col_keywords = ["weekday", "weekend"], title = "", save_dir = ""):
     """
--- src/forecaster/utils.py
+++ src/forecaster/utils.py
@@ -25,22 +25,3 @@ def get_safe_filename(filename):
     - str: The sanitized filename.
     """
     return filename.strip().translate(str.maketrans(string.punctuation + " ", '_' * (len(string.punctuation) + 1))).lower()
-
-def save_figure(fig, filename, save_dir):
-    """
-    Saves a figure to a file.
-
-    Args:
-    - fig (matplotlib.figure.Figure): The figure to save.
-    - filename (str): The name of the file to save the figure to.
-    - save_dir (str): The directory to save the figure to (default is None).
-    """
-    filename = get_safe_filename(filename)
-    if save_dir is not None:
-        save_path = os.path.join(save_dir, filename)
-    else:
-        save_path = filename
-    fig.savefig(save_path, bbox_inches='tight')
-
-
-############################
